5cedfa9c3a377bcb6b2982bf56ff09d95e0f6b7d
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-12-07 19:57:47 +0700

    refactor: structuring and add resources

--- src/application/TurnMove.java
+++ src/application/TurnMove.java
@@ -1,15 +1,26 @@
 package application;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.VBox;
 
 public class TurnMove {
 	
-	
 	private boolean playerTurn = true;
+	
+	private final static String TURNOFF_PATH = "application/resources/turn_off.png";
+	private final static String TURNON_PATH = "application/resources/turn_on.png";
 	
 	public boolean getPlayerTurn() {
 		return playerTurn;
@@ -25,8 +36,22 @@ public class TurnMove {
 		box.setTranslateX(CheckersApp.WIDTH * CheckersApp.TILE_SIZE);
 		box.setPrefHeight(CheckersApp.HEIGHT * CheckersApp.TILE_SIZE);
 		box.setPrefWidth(150);
-		
+
+		ImageView markerOff = new ImageView(TURNOFF_PATH);
+		ImageView markerOn = new ImageView(TURNON_PATH);
+			
+		box.getChildren().add(markerOff);
+			
 		box.getChildren().add(createBtn());
+			
+		box.getChildren().add(markerOn);
+		
+		box.setSpacing(20);
+		
+		BackgroundImage bgImg = new BackgroundImage(new Image("application/resources/black.png"),
+				BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, null);
+		box.setBackground(new Background(bgImg));
+			
 		
 		box.setAlignment(Pos.CENTER);
 		
