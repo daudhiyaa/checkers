2088e96a0dd1b87fb57a5cf0603a73880166198d
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-12-07 18:15:01 +0700

    feat: add vbox and button
    add vertical box, and switch button to root

--- src/application/TurnMove.java
+++ src/application/TurnMove.java
@@ -1,15 +1,12 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
 public class TurnMove {
-	
-	public static VBox turnUI() {
-		VBox box = new VBox();
-		
-//		#Continued After
-		return box;
-	}
 	
 	
 	private boolean playerTurn = true;
@@ -20,6 +17,37 @@ public class TurnMove {
 	
 	public void changeTurn() {
 		playerTurn = !playerTurn;
+	}
+	
+	public VBox turnUI() {
+		VBox box = new VBox();
+		
+		box.setTranslateX(CheckersApp.WIDTH * CheckersApp.TILE_SIZE);
+		box.setPrefHeight(CheckersApp.HEIGHT * CheckersApp.TILE_SIZE);
+		box.setPrefWidth(150);
+		
+		box.getChildren().add(createBtn());
+		
+		box.setAlignment(Pos.CENTER);
+		
+		return box;
+	}
+	
+	private Button createBtn() {
+		Button btn = new Button();
+        btn.setText("Switch");
+		
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+            	changeTurn();
+            	if (playerTurn) {
+            		
+            	}
+            	else ;
+            }
+        });
+		
+		return btn;
 	}
 	
 }
