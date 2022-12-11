6b7d37e810a468fd909f911324a2a1db8217ea84
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-12-11 23:43:57 +0700

    feat: add computer move mechanism
    add temporary computer mode mechanism

--- src/application/CheckersApp.java
+++ src/application/CheckersApp.java
@@ -1,5 +1,9 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Parent;
@@ -8,6 +12,9 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class CheckersApp extends Application {
+	
+	// comp mech
+	private ArrayList<Piece>redPiece = new ArrayList<Piece>();
 
     public static final int TILE_SIZE = 75;
     public static final int WIDTH = 8;
@@ -36,6 +43,9 @@ public class CheckersApp extends Application {
 
                 if (y <= 2 && (x + y) % 2 != 0) {
                     piece = makePiece(PieceType.RED, x, y);
+                    
+                 // comp mech
+                    redPiece.add(piece);
                 }
 
                 if (y >= 5 && (x + y) % 2 != 0) {
@@ -97,6 +107,9 @@ public class CheckersApp extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Scene scene = new Scene(createContent());
+        
+//        createGameLoop();
+        
         primaryStage.setTitle("CheckersApp");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -134,6 +147,7 @@ public class CheckersApp extends Application {
                     board[newX][newY].setPiece(piece);
                     
                     turn.changeTurn();
+                    computerMechanism();
                     break;
                 case KILL:
                     piece.move(newX, newY);
@@ -144,6 +158,10 @@ public class CheckersApp extends Application {
                     board[toBoard(otherPiece.getOldX())][toBoard(otherPiece.getOldY())].setPiece(null);
                     pieceGroup.getChildren().remove(otherPiece);
                     
+                    // comp mech
+                    
+                    redPiece.remove(otherPiece);
+                    
                     break;
             }
             
@@ -152,12 +170,109 @@ public class CheckersApp extends Application {
             	piece.changeToKing();
             	
             	turn.changeTurn();
+            	computerMechanism();
             }
             
-            System.out.println(piece.getIsKing());
         });
 
         return piece;
+    }
+
+    
+//    private void createGameLoop() {
+//		AnimationTimer gameTimer = new AnimationTimer() {
+//
+//			@Override
+//			public void handle(long now) {
+//				if(!turn.getPlayerTurn()) {
+//					computerMechanism();
+//				}
+//			}
+//			
+//		};
+//		gameTimer.start();
+//	}
+//    temporary computer mechanism
+    private void computerMechanism() {
+    	
+    	Piece pieceMove;
+    	int point = 0;
+    	int xNew = 0;
+    	int yNew = 0;
+    	
+    	System.out.println("Computer");
+    	
+    	ArrayList<Integer> combi =new ArrayList<Integer>();
+		combi.add(1);
+		combi.add(-1);
+		combi.add(2);
+		combi.add(-2);
+    	
+    	for(Piece piece : redPiece) {
+    		int temp;
+    		int xNewTemp;
+    		int yNewTemp;
+    		
+            for(int combiX:combi) {
+            	for (int combiY:combi) {
+            		int x0 = toBoard(piece.getOldX());
+                    int y0 = toBoard(piece.getOldY());
+            		xNewTemp = (int) (x0 + combiX);
+                	yNewTemp = (int) (y0 + combiY);
+                	
+                	System.out.println(xNewTemp +" " + yNewTemp);
+                	
+                	MoveResult result;
+                	
+                	if (xNewTemp < 0 || yNewTemp < 0 || xNewTemp >= WIDTH || yNewTemp >= HEIGHT) {
+                        result = new MoveResult(MoveType.NONE);
+                    } else {
+                        result = tryMove(piece, xNewTemp, yNewTemp);
+                    }
+                	
+                	if(result.getType() != MoveType.NONE) {
+
+                        switch (result.getType()) {
+                            case NONE:
+                                piece.abortMove();
+                                break;
+                            case NORMAL:
+                                piece.move(xNewTemp, yNewTemp);
+                                board[x0][y0].setPiece(null);
+                                board[xNewTemp][yNewTemp].setPiece(piece);
+                                
+                                turn.changeTurn();
+                                break;
+                            case KILL:
+                                piece.move(xNewTemp, yNewTemp);
+                                board[x0][y0].setPiece(null);
+                                board[xNewTemp][yNewTemp].setPiece(piece);
+
+                                Piece otherPiece = result.getPiece();
+                                board[toBoard(otherPiece.getOldX())][toBoard(otherPiece.getOldY())].setPiece(null);
+                                pieceGroup.getChildren().remove(otherPiece);
+                                
+                                // comp mech
+                                
+                                redPiece.remove(otherPiece);
+                                
+                                turn.changeTurn();
+                                break;
+                        }
+                        
+                        if((yNewTemp == HEIGHT-1 ) 
+                        		&& (!piece.getIsKing())) {
+                        	piece.changeToKing();
+                        	
+                        	turn.changeTurn();
+                        }
+                		             		
+                		return;
+                	}
+            	}
+            }
+    		
+    	}
     }
 
     public static void main(String[] args) {
