5b4f4939fb42bdd218299a2bd6678b0d2ccb0dad
Keyisa Raihan Illah Setiadi <keyisakris@gmail.com>
2022-12-16 15:46:52 +0700

    fix: win text

--- src/application/game/GameBase.java
+++ src/application/game/GameBase.java
@@ -163,11 +163,10 @@ public class GameBase {
             }
             
             if(GameResult!=0) {
-            	System.out.println(GameResult);
             	if(GameResult==1) {
-            		System.out.println("Red WIN");
+            		resScene.setTxt("Red Win!");
             	}else if(GameResult==2) {
-            		System.out.println("White WIN");
+            		resScene.setTxt("White Win!");
             	}
         		root.getChildren().add(resScene);
             }
